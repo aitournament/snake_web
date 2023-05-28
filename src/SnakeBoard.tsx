cd5e1c75daf86327344c58aa268560e21f5b8c44
Nathan Fox <fuchsnj@gmail.com>
2023-05-27 20:48:57 -0400

    add health indicator

--- src/SnakeBoard.tsx
+++ src/SnakeBoard.tsx
@@ -31,22 +31,91 @@ export interface BoardState {
 export interface Snake {
   id: number,
   team_id: number,
-  body: Pos[]
+  body: Pos[],
+  health: number
 }
 
 interface SnakeBoardProps {
   state: BoardState,
 }
 
+function bodyConnector(
+  color: string,
+  x1: number,
+  y1: number,
+  x2: number,
+  y2: number,
+  percent: number,
+): JSX.Element {
+  let min_x = Math.min(x1, x2);
+  let max_x = Math.max(x1, x2);
+  let min_y = Math.min(y1, y2);
+  let max_y = Math.max(y1, y2);
+
+  let thickness = TILE_SIZE * 0.5 * percent;
+
+  if (y1 == y2) {
+    // horizontal
+    return <rect
+      x={min_x * TILE_SIZE + TILE_SIZE * 0.5}
+      y={min_y * TILE_SIZE + TILE_SIZE * 0.5 - thickness / 2}
+      width={TILE_SIZE}
+      height={thickness}
+      fill={color}
+    />;
+  } else {
+    return <rect
+      x={min_x * TILE_SIZE + TILE_SIZE * 0.5 - thickness / 2}
+      y={min_y * TILE_SIZE + TILE_SIZE * 0.5}
+      width={thickness}
+      height={TILE_SIZE}
+      fill={color}
+    />;
+  }
+
+  
+}
+
 export default function SnakeBoard(props: SnakeBoardProps) {
   console.log('State: ', props.state);
+
+  let elements = [];
+  for(let snake_i = 0; snake_i < props.state.snakes.length; snake_i += 1) {
+    let snake = props.state.snakes[snake_i];
+    let color = snake.team_id == 0 ? 'red': 'blue';
+
+    for(let body_i = 0; body_i < snake.body.length; body_i += 1) {
+      let size = body_i == 0? 0.7 : 0.5;
+      let {x, y} = snake.body[body_i];
+
+      // let cx = x * TILE_SIZE + TILE_SIZE / 2;
+      // let cy = y * TILE_SIZE + TILE_SIZE / 2;
+
+      elements.push(<circle
+        key={`${x}.${y}`}
+        cx={x * TILE_SIZE + TILE_SIZE / 2}
+        cy={y * TILE_SIZE + TILE_SIZE / 2}
+        r={(TILE_SIZE / 2) * size}
+        fill={color}/>);
+
+      if (body_i > 0) {
+        // add body connector
+        let prev_body = snake.body[body_i - 1];
+        let prev_x = prev_body.x;
+        let prev_y = prev_body.y;
+        elements.push(bodyConnector(color, prev_x, prev_y, x, y, snake.health / 100));
+      }
+    }
+  }
+
+
   return <svg
       width={WIDTH * TILE_SIZE}
       height={HEIGHT * TILE_SIZE}
       style={{border: '1px solid gray'}}
   >
     {props.state.food.map((food, i) => {
-      let color = food.healthValue >= 0 ? "green" : "yellow";
+      let color = food.healthValue > 0 ? "green" : "yellow";
       let size = Math.max((Math.abs(food.healthValue) / 50) * 0.7, 0.20);
 
       return <>  
@@ -59,17 +128,6 @@ export default function SnakeBoard(props: SnakeBoardProps) {
           
       </>;
     })}
-    {props.state.snakes.map((snake, snake_i) => {
-      let color = snake.team_id == 0 ? 'red': 'blue';
-      return snake.body.map(({x,y}, body_i) => {
-        let size = body_i == 0? 0.7 : 0.5;
-        return <circle
-            key={`${x}.${y}`}
-            cx={x * TILE_SIZE + TILE_SIZE / 2}
-            cy={y * TILE_SIZE + TILE_SIZE / 2}
-            r={(TILE_SIZE / 2) * size}
-            fill={color}/>;
-      });
-    })}
+    { elements }
   </svg>;
 }
